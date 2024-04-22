aacdb84fbfce5d0e581e0c34d8b9c90f70b679e4
Davon Wilson <davontech3@gmail.com>
2024-04-22 19:30:39 -0400

    setup server

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+const lo = require("lodash");
+const Schema = mongoose.Schema;
+const collections = require("./collections");
+const fs = require('fs');
+const cors = require('cors');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(cors({
+  origin: [ 'http://localhost:3000'], 
+  credentials: true,
+}));
+
+mongoose.connect("mongodb+srv://davontech3:Freshman2@cluster1.1egbhiy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const imageSchema = new Schema({
+  id: Number,
+  name: String,
+  path: String,
+  gender: String,
+  type: String,
+  price: Number
+});
+
+const Image = mongoose.model('clothes', imageSchema);
+
+const colArray = [];
+
+for (let i = 0; i < collections.images.length; i++) {
+  const col = new Image();
+  col.id = collections.images[i].id;
+  col.name = collections.images[i].name;
+  col.path = collections.images[i].path;
+  col.gender = collections.images[i].gender;
+  col.type = collections.images[i].type;
+  col.price = collections.images[i].price;
+  colArray[i] = col;
+}
+Image.find({}).then(foundImages => {
+  if (foundImages.length < 54) {
+    Image.insertMany(colArray)
+      .then(() => {
+        console.log("success");
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+}).catch(err => {
+  console.error(err);
+});
+
+app.post("/image", function(req, res) {
+  const query = Image.where({ name: req.body.name });
+  query.findOne()
+    .then(image => {
+      const path = image.path;
+      const type = image.type;
+      const price = image.price;
+      res.json({
+        type: type,
+        source: path,
+        price: price
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An error occurred' });
+    });
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'), function(err) {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+  });
+});
+
+app.listen(10000, () => {
+  console.log('Server is running on port 10000');
+});
---
+++ collections.js
@@ -0,0 +1,478 @@
+var images = [
+{
+  id: 1,
+  name : "blue designed jeans",
+  path : "/images/bluedesign-p.jpg",
+  gender :"Men",
+  type:"pants",
+  price:100.00
+},
+{
+    id: 2,
+  name : "blue faded jeans",
+  path : "/images/bluefade-p.jpg",
+  gender :"Men",
+  type:"pants",
+  price:75.00
+},
+{
+    id: 3,
+  name : "blue acid wash jeans",
+  path : "/images/acidblue-p.jpg",
+  gender :"Men",
+  type:"pants",
+  price:82.00
+
+},
+{
+    id: 4,
+  name : "blueX designed jeans",
+  path : "/images/designx-p.jpg",
+  gender : "Men",
+  type:"pants",
+  price:130.00
+
+},
+{
+    id: 5,
+  name : "acid wash red jeans",
+  path : "/images/acidred-p.jpg",
+  gender :"Men",
+  type:"pants",
+  price:93.00
+
+},
+{
+    id: 6,
+  name : "white ripped jeans",
+  path : "/images/whiterip-p.jpg",
+  gender :"Men",
+  type:"pants",
+  price:59.00
+
+},
+{
+    id: 7,
+  name : "white jeans",
+  path : "/images/white-p.jpg",
+  gender :"Men",
+  type:"pants",
+  price:65.00
+
+},
+
+
+{
+    id: 8,
+  name : "dark blue jeans",
+  path : "/images/darkblue-p.jpg",
+  gender :"Men",
+  type:"pants",
+  price:84.00
+},
+{
+    id: 9,
+  name : "pink kakkie shorts",
+  path : "/images/pink-sh.jpg",
+  gender :"Men",
+  type:"shorts",
+  price:68.00
+
+},
+{
+    id: 10,
+  name : "white cargo shorts",
+  path : "/images/white-sh.jpg",
+  gender :"Men",
+  type:"shorts",
+  price:78.00
+
+},
+{
+    id: 11,
+  name : "gray cargo shorts",
+  path : "/images/gray-sh.jpg",
+  gender :"Men",
+  type:"shorts",
+  price:47.00
+
+},
+{
+    id: 12,
+  name : "army cargo shorts",
+  path : "/images/army-sh.jpg",
+  gender :"Men",
+  type:"shorts",
+  price:39.00
+},
+{
+    id: 13,
+  name : "badge cargo shorts",
+  path : "/images/badge-sh.jpg",
+  gender :"Men",
+  type:"shorts",
+  price:60.00
+
+},
+{
+    id: 14,
+  name : "blue t-shirt",
+  path : "/images/blue.jpg",
+  gender :"Men",
+  type:"shirts",
+  price:37.00
+},
+{
+id: 15,
+name : "yellow striped shirt",
+path : "/images/yellowStrip-s.jpg",
+gender :"Men",
+type:"shirts",
+price:77.00
+},
+{
+id: 16,
+name : "badge collared shirt",
+path : "/images/badge-s.jpg",
+gender :"Men",
+type:"shirts",
+price:90.00
+},
+{
+id: 17,
+name : "orange t-shirt",
+path : "/images/orange-s.jpg",
+gender :"Men",
+type:"shirts",
+price:50.00
+},
+{
+id: 18,
+name : "red striped shirt",
+path : "/images/striped-s.jpg",
+gender :"Men",
+type:"shirts",
+price: 62.00
+
+},
+{
+id: 19,
+name : "red collared shirt",
+path : "/images/redcollar-s.jpg",
+gender :"Men",
+type:"shirts",
+price: 90.00
+
+},
+{
+id: 20,
+name : "blue plaid shirt",
+path : "/images/plaidblue-s.jpg",
+gender :"Men",
+type:"shirts",
+price: 105.00
+},
+{
+id: 21,
+name : "yellow blouse",
+path : "/images/yellowblouse-s.jpg",
+gender :"Women",
+type:"shirts",
+price: 99.00
+},
+{
+id: 22,
+name : "white long-sleeve shirt",
+path : "/images/longwhite-s.jpg",
+gender :"Women",
+type:"shirts",
+price:45.00
+},
+{
+id: 3,
+name : "dark pink shirt",
+path : "/images/darkpink-s.jpg",
+gender :"Women",
+type:"shirts",
+price:80.00
+
+},
+{
+id: 24,
+name : "pink shirt",
+path : "/images/pink-s.jpg",
+gender :"Women",
+type:"shirts",
+price:25.00
+},
+{
+id: 25,
+name : "pink collar shirt",
+path : "/images/pinkcollar-s.jpg",
+gender :"Women",
+type:"shirts",
+price:59.00
+
+},
+{
+id: 26,
+name : "blue blouse",
+path : "/images/blueblouse-s.jpg",
+gender :"Women",
+type:"shirts",
+price:85.00
+},
+{
+id: 27,
+name : "pink striped shirt",
+path : "/images/pinkstrip-s.jpg",
+gender :"Women",
+type:"shirts",
+price:79.00
+
+},
+{
+id: 28,
+name : "mud designed shirt",
+path : "/images/muddesign-s.jpg",
+gender :"Women",
+type:"shirts",
+price:86.00
+},
+{
+id: 29,
+name : "purple blouse",
+path : "/images/purpleblouse-s.jpg",
+gender :"Women",
+type:"shirts",
+price:109.00
+
+},
+{
+id: 30,
+name : "blue collared shirt",
+path : "/images/bluecollar-s.jpg",
+gender :"Women",
+type:"shirts",
+price:94.00
+
+},
+{
+id: 31,
+name : "purple collared shirt",
+path : "/images/purplecollar-s.jpg",
+gender :"Women",
+type:"shirts",
+price:93.00
+
+},
+{
+id: 32,
+name : "orange summer dress",
+path : "/images/orange-d.jpg",
+gender :"Women",
+type:"dress",
+price:99.00
+},
+{
+id: 33,
+name : "blue plaid dress",
+path : "/images/blueplaid-d.jpg",
+gender :"Women",
+type:"dress",
+price:100.00
+
+},
+{
+id: 34,
+name : "red pokadot dress",
+path : "/images/redpoka-d.jpg",
+gender :"Women",
+type:"dress",
+price:120.00
+
+},
+{
+id: 35,
+name : "black dress",
+path : "/images/black-d.jpg",
+gender :"Women",
+type:"dress",
+price:104.00
+
+},
+{
+id: 36,
+name : "blue stripped dress",
+path : "/images/bluestriped-d.jpg",
+gender :"Women",
+type:"dress",
+price:111.00
+
+},
+{
+id: 37,
+name : "blue summer dress",
+path : "/images/bluesummer-d.jpg",
+gender :"Women",
+type:"dress",
+price:95.00
+
+},
+{
+id: 38,
+name : "acid wash blue jeans",
+path : "/images/acidblue-pw.jpg",
+gender :"Women",
+type:"pants",
+price:91.00
+
+},
+{
+id: 39,
+name : "black skinny jeans",
+path : "/images/blackskinny-pw.jpg",
+gender :"Women",
+type:"pants",
+price:61.00
+
+},
+{
+id: 40,
+name : "red skinny jeans",
+path : "/images/redskinny-pw.jpg",
+gender :"Women",
+type:"pants",
+price:77.00
+
+},
+{
+id: 41,
+name : "black designed jeans",
+path : "/images/blackdesign-pw.jpg",
+gender :"Women",
+type:"pants",
+price:81.00
+
+},
+{
+id: 42,
+name : "white ripped jeans",
+path : "/images/whiterip-pw.jpg",
+gender :"Women",
+type:"pants",
+price:94.00
+
+},
+{
+id: 43,
+name : "white capris",
+path : "/images/whitecapri-pw.jpg",
+gender :"Women",
+type:"pants",
+price:47.00
+
+},
+{
+id: 44,
+name : "teal capris",
+path : "/images/tealcapri-pw.jpg",
+gender :"Women",
+type:"pants",
+price:49.00
+
+},
+{
+id: 45,
+name : "badge kakkies",
+path : "/images/badgekakkie-pw.jpg",
+gender :"Women",
+type:"pants",
+price:52.00
+
+},
+{
+id: 46,
+name : "green cargos",
+path : "/images/greencargo-pw.jpg",
+gender :"Women",
+type:"pants",
+price:69.00
+
+},
+{
+id: 47,
+name : "black dress pants",
+path : "/images/blackdressed-pw.jpg",
+gender :"Women",
+type:"pants",
+price:108.00
+
+},
+{
+id: 48,
+name : "gray dress pants",
+path : "/images/graydressed-pw.jpg",
+gender :"Women",
+type:"pants",
+price:103.00
+
+},
+{
+id: 49,
+name : "blue bell bottoms",
+path : "/images/bluebellbottom-pw.jpg",
+gender :"Women",
+type:"pants",
+price:82.00
+
+},
+{
+id: 50,
+name : "light blue ripped jeans",
+path : "/images/lightbluerip-pw.jpg",
+gender :"Women",
+type:"pants",
+price:60.00
+
+},
+{
+id: 51,
+name : "acid wash ripped blue jeans",
+path : "/images/acidbluerip-pw.jpg",
+gender :"Women",
+type:"pants",
+price:100.00
+
+},
+{
+id: 52,
+name : "pink skinny jeans",
+path : "/images/pinkskinny-pw.jpg",
+gender :"Women",
+type:"pants",
+price:84.00
+
+},
+{
+id: 53,
+name : "purple jeans",
+path : "/images/purple-pw.jpg",
+gender :"Women",
+type:"pants",
+price:50.00
+
+},
+{
+id: 54,
+name : "pink wide pants",
+path : "/images/pinkwide-pw.jpg",
+gender :"Women",
+type:"pants",
+price:52.00
+
+}
+
+]
+
+exports.images= images;
